2d37d07d990d72a5efc5dc2dd9fec8651522d874
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 00:01:35 +0200

    initial commit

---
+++ pages/_document.js
@@ -0,0 +1,46 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+
+    render() {
+        return (
+            <Html lang='pl'>
+                <Head>
+                    <meta charset="UTF-8" />
+                    <meta
+                        name="description"
+                        content="Cześć! Jestem Julka. Nauczę Cię jeść zdrowo - z głową. Ułożę dla Ciebie jadłospis i pomogę Ci w zmianie nawyków żywieniowych."
+                    />
+                    <meta
+                        name="robots"
+                        content="noarchive, max-snippet:-1, max-image-preview:standard"
+                    />
+                    <meta
+                        name="thumbnail"
+                        content="https://cdn.julka.fit/google-thumbnail.jpg"
+                    />
+                    <link rel="shortcut icon" href="https://cdn.julka.fit/favicon.png" />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta property="og:title" content="Julia Migdalska - Dietetyka" />
+                    <meta property="og:type" content="website" />
+                    <meta property="og:url" content="https://julka.fit" />
+                    <meta
+                        property="og:image"
+                        content="https://cdn.julka.fit/og-thumbnail.jpg"
+                    />
+                    <meta
+                        property="og:description"
+                        content="Cześć! Jestem Julka. Nauczę Cię jeść zdrowo - z głową."
+                    />
+                    <link rel="canonical" href="https://julka.fit/" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument;
