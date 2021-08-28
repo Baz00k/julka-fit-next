eef460bd2007cab430c64e77dba320e61da5e985
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 00:55:41 +0200

    add purgecss with config

---
+++ postcss.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    plugins:
+        process.env.NODE_ENV === 'production'
+            ? [
+                "postcss-flexbugs-fixes",
+                [
+                  "postcss-preset-env",
+                  {
+                    autoprefixer: { flexbox: "no-2009" },
+                    stage: 3,
+                    features: { "custom-properties": false }
+                  }
+                ],
+                [
+                  "@fullhuman/postcss-purgecss",
+                  {
+                    content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}", "./components**/*.{js,jsx,ts,tsx}"],
+                    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+                    safelist: ["html", "body"]
+                  }
+                ]
+              ]
+            : [
+                'postcss-flexbugs-fixes',
+                [
+                    'postcss-preset-env',
+                    {
+                        autoprefixer: {
+                            flexbox: 'no-2009',
+                        },
+                        stage: 3,
+                        features: {
+                            'custom-properties': false,
+                        },
+                    },
+                ],
+            ],
+}
