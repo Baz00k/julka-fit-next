f47d7882a04629632192b4abd544609722f60ed9
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 17:12:35 +0200

    add footer

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 text-center">
+            <p>&copy; {new Date().getFullYear().toString()} JULKA.FIT</p>
+        </footer>
+    );
+}
+
+export default Footer;
