50566bf415d952da1de00c151ba28425a4858113
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 23:43:32 +0200

    placeholder for shop

---
+++ pages/sklep/index.js
@@ -0,0 +1,11 @@
+import ShopLayout from "../../layouts/shopLayout";
+
+function Shop() {
+    return (
+        <ShopLayout>
+            <h1 className='text-center'>Sklep</h1>
+        </ShopLayout>
+    );
+}
+
+export default Shop;
