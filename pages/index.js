d7a845f916ee11a87066b3ff69a1e528e174afbc
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 23:43:14 +0200

    content update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import DefaultLayout from "../layouts/defaultLayout"
 import IndexBanner from "../components/indexBanner"
+import SectionWithTitle from "../components/sectionWithTitle"
+import TransparentButton from "../components/transparentButton"
 
 export default function Home() {
   return (
     <DefaultLayout>
-      <IndexBanner/>
+      <IndexBanner />
+      <SectionWithTitle title="O mnie">
+        <p>
+          Nazywam&nbsp;się Julka&nbsp;Migdalska.<br />Pomagam kobietom zmieniać
+          nawyki żywieniowe i&nbsp;uczę jak jeść&nbsp;zdrowo,
+          bez&nbsp;zbędnych&nbsp;restrykcji i&nbsp;wyrzutów&nbsp;sumienia.<br />Wolny
+          czas spędzam w&nbsp;kuchni&nbsp;-&nbsp;gotując i&nbsp;fotografując
+          jedzenie.
+        </p>
+        <TransparentButton href="/o-mnie">Poznaj mnie lepiej!!!</TransparentButton>
+      </SectionWithTitle>
     </DefaultLayout>
   )
 }
