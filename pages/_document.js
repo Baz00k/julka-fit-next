bb23c546a19cdd3eb6aa89d55cb9c35eeaa376b7
Baz00k <gustav.buzuk@gmail.com>
2021-08-29 12:54:04 +0200

    fix charset

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ class MyDocument extends Document {
         return (
             <Html lang='pl'>
                 <Head>
-                    <meta charset="UTF-8" />
+                    <meta charSet="UTF-8" />
                     <meta
                         name="description"
                         content="Cześć! Jestem Julka. Nauczę Cię jeść zdrowo - z głową. Ułożę dla Ciebie jadłospis i pomogę Ci w zmianie nawyków żywieniowych."
