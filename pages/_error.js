b6416f79dde0c9c773b9e1f430ff1fe22ca4ff86
Baz00k <gustav.buzuk@gmail.com>
2021-08-30 23:03:38 +0200

    add custom error widget

---
+++ pages/_error.js
@@ -0,0 +1,16 @@
+function Error({ statusCode }) {
+    return (
+        <p>
+            {statusCode
+                ? `Wystąpił błąd serwera! Numer błędu: ${statusCode}`
+                : 'Wystąpił błąd! Odśwież stronę aby kontynuować'}
+        </p>
+    )
+}
+
+Error.getInitialProps = ({ res, err }) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+    return { statusCode }
+}
+
+export default Error
