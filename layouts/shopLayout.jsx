60c4aa063b9e5f25d8ae419905a56612f73623b2
Baz00k <gustav.buzuk@gmail.com>
2021-09-12 21:37:36 +0200

    generate canonical link

--- layouts/shopLayout.jsx
+++ layouts/shopLayout.jsx
@@ -1,12 +1,28 @@
 import Head from 'next/head';
 import MainNavbar from "../components/navbar";
 import Footer from "../components/footer";
+import { useRouter } from 'next/router'
 
-function ShopLayout({ title='Sklep - Julka Migdalska', children }) {
+function ShopLayout({ title = 'Sklep - Julka Migdalska', children }) {
+
+    function getCanonical() {
+        const CANONICAL_DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+
+        const router = useRouter();
+        const _pathSliceLength = Math.min.apply(Math, [
+            router.asPath.indexOf('?') > 0 ? router.asPath.indexOf('?') : router.asPath.length,
+            router.asPath.indexOf('#') > 0 ? router.asPath.indexOf('#') : router.asPath.length
+        ]);
+        console.log(CANONICAL_DOMAIN + router.asPath.substring(0, _pathSliceLength))
+        return `https://${CANONICAL_DOMAIN + router.asPath.substring(0, _pathSliceLength)}/`;
+    }
+
+
     return (
         <>
             <Head>
                 <title>{title}</title>
+                <link rel="canonical" href={getCanonical()} />
             </Head>
             <MainNavbar sticky='top'></MainNavbar>
             <main>
