b98087b2976f97a420e3f6dd9b64e1093201e319
Baz00k <gustav.buzuk@gmail.com>
2021-09-12 15:56:50 +0200

    divide text to paragraphs

--- pages/o-mnie.js
+++ pages/o-mnie.js
@@ -21,34 +21,48 @@ function Omnie() {
                         />
                     </Col>
                     <Col className='my-5'>
-                        <b>Cześć, to ja - Julka Migdalska!</b><br />
-                        Jestem
-                        studentką dietetyki, instruktorką fitness i trenerką
-                        personalną.<br /><br />W wieku 14 lat zachorowałam na zaburzenia
-                        odżywiania, które... dosłownie zmieniły moje życie. Świat zaczął
-                        kręcić się wokół jedzenia, luster i porównywania się do innych,
-                        a głodówki i diety 1000 kcal pojawiały się u mnie na porządku
-                        dziennym.<br /><br />Schudłam do 37 kg, straciłam okres i wtedy
-                        zrozumiałam, że czas najwyższy zawalczyć o swoje zdrowie.<br /><br />
-                        Dzięki cudownym osobom wokół mnie i ogromie włożonej pracy - udało mi
-                        się przytyć kilkanaście kilogramów i odzyskać wolną głowę. Dziś
-                        akceptuję swoje ciało w 100%. Wiem, że to cudowna maszyna, która
-                        działa zdecydowanie lepiej, gdy dostarcza się jej wartościowego
-                        paliwa.<br /><br />
-                        <b>
-                            Uczę kobiety jak traktować siebie dobrze i jeść zdrowo - bez
-                            restrykcji i wyrzutów sumienia. Pomagam zmieniać nawyki
-                            żywieniowe i uświadamiam, że zdrowy rozsądek to podstawa, a w
-                            diecie jest miejsce na wszystko (tak - na pizzę z ciągnącą
-                            mozzarellą i ulubione lody również!)
-                            <br /><br />
-                        </b>
-                        Na co dzień możesz mnie znaleźć na
-                        <a href="https://www.instagram.com/julka_migdalska/" target="_blank" rel="noreferrer"> Instagramie</a>.
-                        Jest to mała społeczność i miejsce, w którym wyżywam się
-                        kulinarnie, rozwijam umiejętności fotografii kulinarnej i szerzę
-                        wiedzę związana ze zdrowym stylem życia.<br /><br />
-                        <Link href="/kontakt">Napisz do mnie</Link> 😊 Pokażę Ci jak jeść zdrowo – z głową!
+                        <p><b>Cześć, to ja - Julka Migdalska!</b></p>
+                        <p>
+                            Jestem studentką dietetyki, instruktorką fitness i trenerką
+                            personalną.
+                        </p>
+                        <p>
+                            W wieku 14 lat zachorowałam na zaburzenia
+                            odżywiania, które... dosłownie zmieniły moje życie. Świat zaczął
+                            kręcić się wokół jedzenia, luster i porównywania się do innych,
+                            a głodówki i diety 1000 kcal pojawiały się u mnie na porządku
+                            dziennym.
+                        </p>
+                        <p>
+                            Schudłam do 37 kg, straciłam okres i wtedy
+                            zrozumiałam, że czas najwyższy zawalczyć o swoje zdrowie.
+                        </p>
+                        <p>
+                            Dzięki cudownym osobom wokół mnie i ogromie włożonej pracy - udało mi
+                            się przytyć kilkanaście kilogramów i odzyskać wolną głowę. Dziś
+                            akceptuję swoje ciało w 100%. Wiem, że to cudowna maszyna, która
+                            działa zdecydowanie lepiej, gdy dostarcza się jej wartościowego
+                            paliwa.
+                        </p>
+                        <p>
+                            <b>
+                                Uczę kobiety jak traktować siebie dobrze i jeść zdrowo - bez
+                                restrykcji i wyrzutów sumienia. Pomagam zmieniać nawyki
+                                żywieniowe i uświadamiam, że zdrowy rozsądek to podstawa, a w
+                                diecie jest miejsce na wszystko (tak - na pizzę z ciągnącą
+                                mozzarellą i ulubione lody również!)
+                            </b>
+                        </p>
+                        <p>
+                            Na co dzień możesz mnie znaleźć na
+                            <a href="https://www.instagram.com/julka_migdalska/" target="_blank" rel="noreferrer"> Instagramie</a>.
+                            Jest to mała społeczność i miejsce, w którym wyżywam się
+                            kulinarnie, rozwijam umiejętności fotografii kulinarnej i szerzę
+                            wiedzę związana ze zdrowym stylem życia.
+                        </p>
+                        <p>
+                            <Link href="/kontakt">Napisz do mnie</Link> 😊 Pokażę Ci jak jeść zdrowo – z głową!
+                        </p>
                     </Col>
                 </Row>
             </Container>
