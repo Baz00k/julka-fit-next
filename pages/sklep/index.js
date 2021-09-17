b598cba48067ad415d59a187706beee3e92f8a09
Baz00k <gustav.buzuk@gmail.com>
2021-09-17 22:00:27 +0200

    add shop layout

--- pages/sklep/index.js
+++ pages/sklep/index.js
@@ -1,11 +1,48 @@
-import ShopLayout from "../../layouts/shopLayout";
+import ShopLayout from '../../layouts/shopLayout'
+import { Container, Row, Col } from 'react-bootstrap'
+import OutlineButton from '../../components/outlineButton'
+import PageTitle from '../../components/pageTitle'
+import ShopItem from '../../components/shopItem'
+import getProductsList from '../../utils/getProductsList'
 
-function Shop() {
-    return (
-        <ShopLayout>
-            <h1 className='text-center'>Sklep</h1>
-        </ShopLayout>
-    );
+function Sklep({ productList }) {
+
+  return (
+    <ShopLayout>
+      <Container>
+        <section className='shop-banner'>
+          <Row xs={1} md={2}>
+            <Col>
+              <h1 className='shop-title font-abhaya display-1'>Sklep</h1>
+              <h2 className='font-abhaya mt-3'>
+                Zakochaj się w swojej diecie i jedz zdrowo, bez zbędnych restrykcji
+                i wyrzutów sumienia.
+              </h2>
+              <OutlineButton href="#products">Chcę zobaczyć produkty</OutlineButton>
+            </Col>
+            <Col>
+              <img src='/looking_left.png' className="shop-banner-image img-fluid" alt="Julia Migdalska" />
+            </Col>
+          </Row>
+        </section>
+        <section className='products'>
+          <PageTitle id="products">Produkty</PageTitle>
+          <Row xs={1} md={2} xl={3}>
+            {productList.map((product) => <ShopItem data={{ ...product }} key={product.id}></ShopItem>)}
+          </Row>
+        </section>
+      </Container>
+    </ShopLayout>
+  )
 }
 
-export default Shop;
+export async function getStaticProps(context) {
+
+  const productList = await getProductsList();
+
+  return {
+    props: { productList: productList.data },
+  }
+}
+
+export default Sklep;
