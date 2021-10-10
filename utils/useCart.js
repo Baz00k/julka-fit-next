1aea3dc97375db764f76ccd4f34d905127cea76b
Baz00k <gustav.buzuk@gmail.com>
2021-10-10 16:46:03 +0200

    add cart functionality

---
+++ utils/useCart.js
@@ -0,0 +1,66 @@
+import { createContext, useContext, useReducer, useEffect } from 'react'
+import { getStorageItem, setStorageItem } from './storage'
+
+const defaultCart = {
+    products: []
+}
+
+export const CartContext = createContext();
+
+export function useCartState() {
+
+    const [cart, dispatch] = useReducer(cartReducer, defaultCart);
+
+    useEffect(() => {
+        dispatch({ type: 'getFromStorage' });
+    }, []);
+
+    function addToCart(id) {
+        dispatch({ type: 'add', payload: id });
+    }
+
+    function removeFromCart(id) {
+        dispatch({ type: 'remove', payload: id });
+    }
+
+    return {
+        cart,
+        addToCart,
+        removeFromCart
+    }
+
+}
+
+function cartReducer(state, action) {
+    let newCart = { ...state };
+    switch (action.type) {
+        case 'add':
+            if (newCart.products.includes(action.payload)) {
+                return newCart;
+            }
+            newCart.products.push(action.payload);
+            setStorageItem('cart', newCart);
+            return newCart;
+        case 'remove':
+            let index = newCart.products.indexOf(action.payload);
+            if (index != -1) {
+                newCart.products.splice(index, 1);
+                setStorageItem('cart', newCart);
+            }
+            return newCart;
+        case 'getFromStorage':
+            let storageCart = getStorageItem('cart');
+            if (storageCart && storageCart.products) {
+                return storageCart;
+            }
+            setStorageItem('cart', defaultCart);
+            return defaultCart;
+        default:
+            throw new Error('Forbidden action for useCart reducer');
+    }
+}
+
+export function useCart() {
+    const cart = useContext(CartContext);
+    return cart;
+}
