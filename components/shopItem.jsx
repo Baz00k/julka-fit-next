d6b405962d685e538067d0f5092ceae84ebd78f0
Baz00k <gustav.buzuk@gmail.com>
2021-10-10 16:41:08 +0200

    add addToCart functionality

--- components/shopItem.jsx
+++ components/shopItem.jsx
@@ -1,17 +1,18 @@
 import { Col, ButtonGroup, Button } from "react-bootstrap"
 import OutlineButton from "../components/outlineButton"
 import { FaShoppingCart } from 'react-icons/fa'
-import addToCart from "../utils/addToCart"
+import { useCart } from '../utils/useCart'
 
 
 function ShopItem({ data }) {
+    const { addToCart } = useCart();
     return (
         <Col className="p-3 text-center d-flex flex-column align-items-center justify-content-start">
             <div className="cover-background">
                 <div className="cover-device" style={{ backgroundImage: `url(${data.images[0]})` }}>
                 </div>
             </div>
-            <h3 className="mt-4 font-abril">{data.name}</h3>
+            <h2 className="h3 mt-4 font-abril">{data.name}</h2>
             <p className="product-price fs-2 mb-4">
                 Cena:{" "}
                 <span style={{ color: "#b6366c" }}>
@@ -20,7 +21,7 @@ function ShopItem({ data }) {
             </p>
             <ButtonGroup className="mt-auto">
                 <OutlineButton href={`/sklep/${data.link}`} >Dowiedz się więcej!</OutlineButton>
-                <Button variant="outline-dark" style={{ minWidth: 0 }} onClick={() => addToCart(data.id)}><FaShoppingCart /></Button>
+                <Button variant="outline-dark" style={{ minWidth: 0 }} onClick={() => addToCart(data.id)} aria-label='add to cart'><FaShoppingCart /></Button>
             </ButtonGroup>
         </Col>
     );
