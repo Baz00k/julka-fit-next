cc4179627ae4b3924e68f1e9e7f85a8b090c1c92
Baz00k <gustav.buzuk@gmail.com>
2021-10-10 22:31:55 +0200

    add cart functionality

--- pages/sklep/koszyk.js
+++ pages/sklep/koszyk.js
@@ -1,9 +1,32 @@
-import WorkInProgress from "../../layouts/wip";
+import ShopLayout from "../../layouts/shopLayout"
+import PageTitle from "../../components/pageTitle"
+import { Container, Row, Col } from "react-bootstrap"
+import CartList from "../../components/cartList"
 
-function Koszyk() {
+import getProductsList from '../../utils/getProductsList'
+
+function Koszyk({ productList }) {
     return (
-       <WorkInProgress />
+        <ShopLayout>
+            <Container>
+                <PageTitle>Koszyk</PageTitle>
+                <Row xs={1} xl={2} className='justify-content-center'>
+                    <Col className="text-center">
+                        <CartList productList={productList} />
+                    </Col>
+                </Row>
+            </Container>
+        </ShopLayout>
     );
 }
 
 export default Koszyk;
+
+export async function getStaticProps(context) {
+
+    const productList = await getProductsList();
+
+    return {
+        props: { productList: productList.data },
+    }
+}
