899f5bd3bc0e2eea8be6718e3180e56aa48904eb
Baz00k <gustav.buzuk@gmail.com>
2021-10-10 16:39:47 +0200

    add cart widget

---
+++ components/cartWidget.jsx
@@ -0,0 +1,33 @@
+import { Nav, Badge } from 'react-bootstrap'
+import Link from 'next/link'
+import { FaShoppingCart } from 'react-icons/fa'
+import { useCart } from '../utils/useCart'
+import { useEffect, useState } from 'react'
+
+function CartWidget() {
+
+    const { cart } = useCart();
+    const [itemCount, setItemCount] = useState(0);
+    let [animate, setAnimate] = useState(false);
+
+    useEffect(() => {
+        setItemCount(cart.products.length || 0);
+        if (!animate) {
+            setAnimate(true);
+            setTimeout(() => setAnimate(false), 500);
+        }
+    }, [cart]);
+
+    return (
+        <Link href={'/sklep/koszyk'} passHref>
+            <Nav.Link className={animate ? 'cart-button animate-cart' : 'cart-button'}>
+                <FaShoppingCart size={30} />
+                <Badge pill bg="dark" className='cart-badge'>
+                    {itemCount}
+                </Badge>
+            </Nav.Link>
+        </Link>
+    );
+}
+
+export default CartWidget;
