707035bc6d827983ac739fe0dee92af422b8aa96
Baz00k <gustav.buzuk@gmail.com>
2021-10-10 16:42:37 +0200

    provide context for cart

--- layouts/shopLayout.jsx
+++ layouts/shopLayout.jsx
@@ -1,20 +1,25 @@
 import Head from 'next/head'
 import getCanonical from '../utils/getCanonical'
-import MainNavbar from "../components/navbar"
+import { CartContext, useCartState } from '../utils/useCart'
+import ShopNavbar from "../components/shopNavbar"
 import CookieConsent from '../components/cookieConsent'
 import Footer from "../components/footer"
 
 function ShopLayout({ title = 'Sklep - Julka Migdalska', children }) {
+    const cart = useCartState();
+
     return (
         <>
             <Head>
                 <title>{title}</title>
                 {getCanonical()}
             </Head>
-            <MainNavbar sticky='top'></MainNavbar>
-            <main>
-                {children}
-            </main>
+            <CartContext.Provider value={cart}>
+                <ShopNavbar sticky='top'></ShopNavbar>
+                <main>
+                    {children}
+                </main>
+            </CartContext.Provider>
             <CookieConsent />
             <Footer></Footer>
         </>
