0c8292f80b488dfab154ea61d4406efbad7c2fb3
Baz00k <gustav.buzuk@gmail.com>
2021-11-04 03:42:39 +0100

    add noopener to link

--- components/socialLink.jsx
+++ components/socialLink.jsx
@@ -4,7 +4,7 @@ import { Nav } from 'react-bootstrap'
 function NavLink({ href, iconURL, altTxt }) {
     return (
         <Link href={href} passHref>
-            <Nav.Link target="_blank" rel="noreferrer">
+            <Nav.Link target="_blank" rel="noopener noreferrer">
                 <img
                     src={iconURL}
                     alt={altTxt}
