ceb474c88883c312c06a1e972420fdaa74c48351
Baz00k <gustav.buzuk@gmail.com>
2021-11-06 19:59:44 +0100

    fix and display errors

--- components/paymentForm.jsx
+++ components/paymentForm.jsx
@@ -1,10 +1,82 @@
-import { Form } from 'react-bootstrap'
+import { useRef, useState } from 'react'
+import { Form, Spinner, Alert } from 'react-bootstrap'
 import OutlineButton from './outlineButton'
-import Link from 'next/link';
+import getStripe from '../utils/getStripe'
 
 function PaymentForm({ productID }) {
+
+    const form = useRef(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState({ message: false });
+
+    async function submit(e) {
+        setLoading(true);
+        e.preventDefault();
+
+        //get data from form
+        var data = {
+            "productID": productID,
+            "name": form.current.name.value,
+            "email": form.current.email.value.toLowerCase(),
+            "promo_code": form.current.coupon.value,
+        }
+
+        //send data to server
+        const response = await fetch('https://jdkkz9sbhd.execute-api.eu-central-1.amazonaws.com/default', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+            body: JSON.stringify(data || {})
+        }).catch(err => {
+            console.log(err);
+            return { ok: false };
+        });
+
+        //check if response was successful
+        if (response.ok) {
+
+            const json = await response.json();
+
+            //check if coupon was valid
+            if (json.error) {
+
+                setError({ message: json.error });
+                if (json.errorMessage) {
+                    console.log(json.errorMessage);
+                }
+
+            } else {
+
+                try {
+                    const stripe = await getStripe();
+                    const { error } = await stripe.redirectToCheckout({
+                        sessionId: json.session.id,
+                    });
+                    if (error) {
+                        setError({ message: error.message });
+                    }
+                } catch (error) {
+                    console.log(error);
+                    setError({ message: "Wystąpił błąd płatności, odśwież stronę i spróbuj ponownie" });
+                }
+
+            }
+
+        } else {
+            setError({ message: "Wystąpił błąd połączenia z serwerem, odśwież stronę i spróbuj ponownie" });
+        }
+
+        setLoading(false);
+    }
+
     return (
-        <Form method='post' action="https://api.sklep.julka.fit/payment">
+        <Form encType="application/json" ref={form} onSubmit={submit} >
             <div className="mb-3">
                 <p>
                     <b>Wpisz swoje imię i e-mail na który zostanie wysłany produkt</b>
@@ -16,7 +88,7 @@ function PaymentForm({ productID }) {
             </Form.Group>
             <Form.Group controlId='email' className="mb-3">
                 <Form.Label>E-mail</Form.Label>
-                <Form.Control type='email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" name='email' placeholder='Wpisz swój e-mail!' required />
+                <Form.Control type='email' pattern="[A-Za-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" name='email' placeholder='Wpisz swój e-mail!' required />
             </Form.Group>
             <Form.Group controlId='coupon' className="mb-3">
                 <Form.Label>Kod rabatowy</Form.Label>
@@ -25,7 +97,8 @@ function PaymentForm({ productID }) {
             <Form.Group controlId='consent' className="mb-1">
                 <Form.Check type="checkbox" required inline />
                 <Form.Label className="d-inline">
-                    Potwierdzam zapoznanie się z <Link href="/regulamin" target="_blank">regulaminem</Link> oraz <Link href="/polityka-prywatnosci" target="_blank">polityką&nbsp;prywatności</Link>.
+                    {/* eslint-disable-next-line @next/next/no-html-link-for-pages */}
+                    Potwierdzam zapoznanie się z <a href="/regulamin" target="_blank">regulaminem</a> oraz <a href="/polityka-prywatnosci" target="_blank">polityką&nbsp;prywatności</a>.
                 </Form.Label>
             </Form.Group>
             <Form.Group controlId='consent-link' className="mb-3">
@@ -36,8 +109,17 @@ function PaymentForm({ productID }) {
             </Form.Group>
             <Form.Group controlId='submit' className="mb-3">
                 <Form.Control type='hidden' name="productID" value={productID} />
-                <OutlineButton type='submit' size='lg' className="button-buy">
-                    Kupuję!
+                <OutlineButton type='submit' size='lg' className="button-buy" disabled={loading} >
+                    {
+                        loading ?
+                            <div className="m-1 fs-6">
+                                <Spinner animation="border" role="status" className="text-reset">
+                                    <span className="visually-hidden">Wysyłanie...</span>
+                                </Spinner>
+                            </div>
+                            :
+                            "Kupuję!"
+                    }
                 </OutlineButton>
             </Form.Group>
             <div className="mb-3">
@@ -45,6 +127,12 @@ function PaymentForm({ productID }) {
                     Nie zamykaj okna przeglądarki podczas dokonywania płatności!
                 </p>
             </div>
+            {
+                error.message && <Alert variant="danger">
+                    {error.message}
+                </Alert>
+            }
+
         </Form>
     );
 }
