d8f475390433b2cb64fce627ad425d0571a411eb
Baz00k <gustav.buzuk@gmail.com>
2021-11-07 01:22:51 +0100

    exclude pages from sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,24 @@
 module.exports = {
-    siteUrl: 'https://julka.fit',
-    generateRobotsTxt: true
+    siteUrl: process.env.NEXT_PUBLIC_DOMAIN || 'https://julka.fit',
+    changeFreq: 'monthly',
+    generateRobotsTxt: true,
+    exclude: [
+        '/404',
+        '/500',
+        '/_app',
+        '/_document',
+        '/_error',
+        '/_next',
+        '/_redirects',
+        '/_static',
+        '/_test',
+        '/_test/**',
+        '/_app/**',
+        '/_document/**',
+        '/_error/**',
+        '/_next/**',
+        '/dzieki',
+        '/sklep/platnosc-anulowana',
+        '/sklep/sukces'
+    ],
 }
