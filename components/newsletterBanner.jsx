1ff9212ffb4407dd7170b557b4b072e5b8517c8f
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-02-09 16:40:10 +0100

    feat: add mailer lite newsletter banner

--- components/newsletterBanner.jsx
+++ components/newsletterBanner.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import CenteredModal from './centeredModal'
-import NewsletterForm from './newsletterForm'
-import OutlineButton from './outlineButton'
 
 function NewsletterBanner() {
-
-    const openNewsletterPopup = () => {
-        ml('show', 'tli5iW', true);
+    const mailerLiteButtonHtml = () => {
+        return {
+            __html: '<button class="ml-onclick-form mt-auto btn btn-outline-dark" onclick="ml(\'show\', \'tli5iW\', true)">Zapisuję się!</button>'
+        }
     }
 
     return (
@@ -17,11 +14,9 @@ function NewsletterBanner() {
                     <Col className='text-md-start d-flex flex-column align-items-center align-items-md-start' xs={12} md={6}>
                         <h3 className='font-abhaya h1'>Mam pewną propozycję!</h3>
                         <p>
-                            Dołącz do mojego newslettera,
-                            odbierz darmowy jadłospis wegetariański na 3 dni i dowiedz się,
-                            jak być &quot;FIT&quot;, jeść pizzę i mieć przy tym wolną głowę!
+                            Dołącz do mojego newslettera, odbierz darmowy jadłospis wegetariański na 3 dni i bądź ze mną w stałym kontakcie!
                         </p>
-                        <OutlineButton className="ml-onclick-form" onClick={openNewsletterPopup}>Zapisuję się!</OutlineButton>
+                        <div dangerouslySetInnerHTML={mailerLiteButtonHtml()}></div>
                     </Col>
                     <Col className='d-none d-md-flex' md={3}>
                         <img src="https://cdn.julka.fit/looking_left.png" alt="Julia Migdalska" className='newsletter-image' />
