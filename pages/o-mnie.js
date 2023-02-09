9ed66e9e1e4ffb3aca597a4c8ada08f4ad7b9eb0
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-02-09 16:39:59 +0100

    chore: change text

--- pages/o-mnie.js
+++ pages/o-mnie.js
@@ -9,8 +9,7 @@ function Omnie() {
             <Container className='justify-content-center'>
                 <Row>
                     <Col xs={12} className='text-center'>
-                        <h1 className='mt-5 font-abhaya'><b>“Zdrowe podejście to podstawa”</b> -&nbsp;</h1>
-                        <h2 className='h4'>ale... czy zawsze tak u mnie było?</h2>
+                        <h1 className='mt-5 font-abhaya fw-bold'>“Zdrowe podejście to podstawa”</h1>
                     </Col>
                     <Col xs={12} md={6} lg={4} xl={3} className="p-5 p-md-3 pt-md-5">
                         <img
@@ -20,28 +19,9 @@ function Omnie() {
                         />
                     </Col>
                     <Col className='my-5'>
-                        <p><b>Cześć, to ja - Julka Migdalska!</b></p>
+                        <p><b>Cześć, miło Cię tu widzieć.</b></p>
                         <p>
-                            Jestem studentką dietetyki, instruktorką fitness i trenerką
-                            personalną.
-                        </p>
-                        <p>
-                            W wieku 14 lat zachorowałam na zaburzenia
-                            odżywiania, które... dosłownie zmieniły moje życie. Świat zaczął
-                            kręcić się wokół jedzenia, luster i porównywania się do innych,
-                            a głodówki i diety 1000 kcal pojawiały się u mnie na porządku
-                            dziennym.
-                        </p>
-                        <p>
-                            Schudłam do 37 kg, straciłam okres i wtedy
-                            zrozumiałam, że czas najwyższy zawalczyć o swoje zdrowie.
-                        </p>
-                        <p>
-                            Dzięki cudownym osobom wokół mnie i ogromie włożonej pracy - udało mi
-                            się przytyć kilkanaście kilogramów i odzyskać wolną głowę. Dziś
-                            akceptuję swoje ciało w 100%. Wiem, że to cudowna maszyna, która
-                            działa zdecydowanie lepiej, gdy dostarcza się jej wartościowego
-                            paliwa.
+                            Mam na imię Julka. Jestem dyplomowaną dietetyczką, fotografką kulinarną i miłośniczką dobrego jedzenia.
                         </p>
                         <p>
                             <b>
