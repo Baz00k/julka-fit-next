8d271d505da10a7d6e8a79d5cd771b17747cef2c
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-02-24 17:22:30 +0100

    chore: update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,7 @@ module.exports = {
         content: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}", "./layouts/**/*.{js,jsx,ts,tsx}", './node_modules/react-bootstrap/**/*.{js,jsx,ts,tsx}'],
         defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
         safelist: {
-          standard: ["html", "body", "navbar", "invalid-feedback", /^nav-/, "container", "col", /row/, "bg-white", "bg-dark", /btn/, "form-check-inline", "spinner-border"],
+          standard: ["html", "body", "navbar", "invalid-feedback", /^nav-/, /container/, "col", /row/, "bg-white", "bg-dark", /btn/, "form-check-inline", "spinner-border", /m-/, /p-/],
           deep: [/^navbar/, /@media/, /col/, /row/, /^btn/, /^modal/, /toast/, /carousel/],
           // greedy: [],
         }
