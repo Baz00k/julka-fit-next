2fec558d7d72ebd85d8f574cd21619fc642a7b48
Jakub Buzuk <61548378+Baz00k@users.noreply.github.com>
2023-03-03 17:24:44 +0100

    feat: add additional product description

--- pages/sklep/wege-student.jsx
+++ pages/sklep/wege-student.jsx
@@ -6,7 +6,7 @@ import PaymentModal from '../../components/paymentModal';
 
 const stripeID = 'price_1MefwOFlitTRS2IiYvEuK5n1';
 const cdnURL = 'wege-student';
-const title = 'wege student';
+const title = 'Wege Student';
 const price = '69.00 zł';
 const desc =
     'Jak wiadomo - student to gatunek z natury biedny, zarobiony i... wiecznie głodny. Wychodzę naprzeciw! Będzie tanio, szybko, odżywczo i baaaardzo smacznie!';
@@ -52,7 +52,7 @@ function WegeStudent() {
                             xl={4}
                             className='col-xl-4 m-xl-4 p-2 gx-0'
                         >
-                            <h2 className='carousel-title'>Co znajdziesz w środku?</h2>
+                            <h3 className='carousel-title'>Co znajdziesz w środku?</h3>
                             <div className='carousel-wrapper'>
                                 <Carousel variant='dark'>
                                     {getCarouselItems(7)}
@@ -88,6 +88,11 @@ function WegeStudent() {
                                 </li>
                                 <li>
                                     Listę zamienników niektórych produktów spożywczych.
+                                </li>
+                                <li>
+                                    2 wersje e-booka. Z wyliczonym makro oraz BEZ
+                                    podanych kcal. Możesz korzystać z której wersji
+                                    chcesz!
                                 </li>
                                 <li>
                                     3-dniowy jadłospis 1800 kcal na bazie przepisów z
@@ -133,10 +138,92 @@ function WegeStudent() {
                         Ten e-book NIE jest dla Ciebie, jeśli...
                     </h3>
                     <ul>
-                        <li>Szukasz wyszukanych, skomplikowanych posiłków i lubisz buszować w kuchni godzinami.</li>
-                        <li>Nie zadowoli Cię prosty przepis z ziemniora, ekspresowy makaron czy naleśniki.</li>
+                        <li>
+                            Szukasz wyszukanych, skomplikowanych posiłków i lubisz
+                            buszować w kuchni godzinami.
+                        </li>
+                        <li>
+                            Nie zadowoli Cię prosty przepis z ziemniora, ekspresowy
+                            makaron czy naleśniki.
+                        </li>
                         <li>Nie lubisz przepisów bez mięsa i ryb.</li>
-                        <li>Nienawidzisz strączków, a gdy widzisz tofu - uciekasz, gdzie pieprz rośnie.</li>
+                        <li>
+                            Nienawidzisz strączków, a gdy widzisz tofu - uciekasz,
+                            gdzie pieprz rośnie.
+                        </li>
+                    </ul>
+                </section>
+
+                <section className='my-5'>
+                    <h2 className='carousel-title'>Spis treści:</h2>
+                    <h3 className='carousel-title-small'>
+                        <u>Wstęp</u>
+                    </h3>
+                    <ul>
+                        <li>Jak oszczędzać kasę na zakupach spożywczych?</li>
+                        <li>Zaoszczędź trochę czasu w kuchni!</li>
+                        <li>Plan minimum</li>
+                        <li>Lista zamienników</li>
+                        <li>Zanim zaczniesz gotować</li>
+                        <li>Pomysły na wartościowe przekąski</li>
+                    </ul>
+
+                    <h3 className='carousel-title-small'>
+                        <u>Szybkie śniadanie... kiedy zajęcia na uczelni zaczynają się o 8:00</u>
+                    </h3>
+                    <ul>
+                        <li>Omlet bananowy 1,2,3,4</li>
+                        <li>Omlet sernikowy z gorącymi wiśniami</li>
+                        <li>Omlet meksykański</li>
+                        <li>Chleb z jajem</li>
+                        <li>Tosty z serem, ale na bogato</li>
+                        <li>Szakszuka po meksykańsku</li>
+                        <li>Tofucznica na zielono</li>
+                        <li>Czekoladowe naleśniory z tofu</li>
+                        <li>Waniliowe placuszki z borówkami</li>
+                        <li>Czekoladowy koktajl mocy</li>
+                        <li>Nocny cynamonowy krem owsiany</li>
+                        <li>Mini rollsy na 2 sposoby</li>
+                        <li>Śniadaniowy omlet w tortilli</li>
+                        <li>Ekspresowa pasta jajeczna</li>
+                        <li>Kanapkowa pasta z cieciorki (bez blendera)</li>
+                        <li>Pasta z tofu i suszonych pomidorów</li>
+                    </ul>
+
+                    <h3 className='carousel-title-small'>
+                        <u>Obiadki... tańsze niż na studenckiej stołówce</u>
+                    </h3>
+                    <ul>
+                        <li>Pomidorowa tania jak barszcz</li>
+                        <li>Błyskawiczna zupka curry</li>
+                        <li>Zupa z &quot;tofuwkładką&quot;</li>
+                        <li>Smażony brązowy ryż z warzywami</li>
+                        <li>Danie z puszki, czyli fasolka w pomidorach</li>
+                        <li>Zielone kluchy</li>
+                        <li>Kozacki makaron na czarną godzinę</li>
+                        <li>Makaron hummusowy</li>
+                        <li>Makaron w kremowym sosie pesto</li>
+                        <li>Makaron w kremowym sosie pomidorowym</li>
+                        <li>Sałatka z kaszy gryczanej</li>
+                        <li>Sałatka w 5 minut</li>
+                        <li>Soczewica w pomidorach</li>
+                        <li>Tofu w sosie słodko-kwaśnym</li>
+                        <li>Paluszki (bez)rybne</li>
+                        <li>Studenckie mielone z tofu</li>
+                        <li>Tofumięsko do wrapów i nie tylko</li>
+                    </ul>
+
+                    <h3 className='carousel-title-small'>
+                        <u>Na weekendy... kiedy czasu jest ciut więcej, ale kasy nadal nie bardzo</u>
+                    </h3>
+                    <ul>
+                        <li>Pomarańczowe naleśniory na maślance</li>
+                        <li>Bananowy pudding z chleba, co go zapomnieli</li>
+                        <li>Zupka mocy</li>
+                        <li>Zupa krem z pora i kalafiora</li>
+                        <li>Pełnoziarniste tortille antyinflacyjne</li>
+                        <li>Wytrawne placuszki za 6 zeta</li>
+                        <li>Ziemniaczana sałatka studencka</li>
                     </ul>
 
                     <div className='d-flex justify-content-center flex-column text-center mt-5'>
@@ -148,6 +235,7 @@ function WegeStudent() {
                         </div>
                     </div>
                 </section>
+
                 <PaymentModal
                     show={modalShow}
                     setModalShow={setModalShow}
